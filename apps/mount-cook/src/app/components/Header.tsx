5e71b071e1d532b9d8ffb1b330a0ec50df4d72d6
Matthew Gong <matthew.gong@wherely.app>
2026-06-01 11:33:52 +1200

    Create the `Header` component

--- apps/mount-cook/src/app/components/Header.tsx
+++ apps/mount-cook/src/app/components/Header.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import { Button } from 'np-westland';
+
 export default function Header() {
-  return <div>Welcome to wherely</div>;
+  return (
+    <div className="flex justify-between bg-white p-1">
+      <div>Wherely</div>
+      <div>
+        <Button>Log in</Button>
+      </div>
+    </div>
+  );
 }
