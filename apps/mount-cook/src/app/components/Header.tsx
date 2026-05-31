d4b9b673416853bbb253088a20b63538de2f081c
Matthew Gong <matthew.gong@wherely.app>
2026-06-01 09:38:35 +1200

    Create the `Header` component

---
+++ apps/mount-cook/src/app/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div>Welcome to wherely</div>;
+}
--- apps/mount-cook/src/app/layout.tsx
+++ apps/mount-cook/src/app/layout.tsx
@@ -1,11 +1,9 @@
+import ConfigureAmplifyClientSide from '@/app/components/ConfigureAmplify';
+import Header from '@/app/components/Header';
 import '@aws-amplify/ui-react/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Link from 'next/link';
 import './globals.css';
-
-import ConfigureAmplifyClientSide from '@/components/ConfigureAmplify';
-import Logout from '@/components/auth/logout/Logout';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,10 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ConfigureAmplifyClientSide />
         <div className="flex h-full flex-col">
-          <div className="h-[100px]">
-            <Logout />
-            <Link href="/map-layout-editor">Map Layout Editor</Link>
-          </div>
+          <Header />
           <div className="relative flex-1">{children}</div>
         </div>
       </body>
