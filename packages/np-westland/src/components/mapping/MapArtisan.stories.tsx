88435f4bfad74954a01cdfd99960cc937c4a71c1
Matthew Gong <matthew.gong@wherely.app>
2026-06-01 18:58:02 +1200

    Make the map artisan stories take up the view port

--- packages/np-westland/src/components/mapping/MapArtisan.stories.tsx
+++ packages/np-westland/src/components/mapping/MapArtisan.stories.tsx
@@ -3,6 +3,16 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 const meta = {
   component: MapArtisan,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div className='fixed inset-0 h-dvh w-dvw overflow-hidden'>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof MapArtisan>;
 
 export default meta;
